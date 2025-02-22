a7748a857d636bbe30cec0bbd744ed19bb31d693
035966-L3 <139025318+035966-L3@users.noreply.github.com>
2025-02-22 18:49:02 +0800

    Add files via upload

---
+++ convert_csv_files.cpp
@@ -0,0 +1,64 @@
+// Step 2: Dispose original CSV leaderboard
+// Command: convert_csv_files [CONTEST_NAME] [CSV_FILENAME]
+#include <bits/stdc++.h>
+using namespace std;
+pair<int, string> dispose(string cur)
+{
+	int place = 0;
+	int pos = 0;
+	while (cur[pos] != ',')
+	{
+		place = place * 10 + cur[pos] - '0';
+		pos++;
+	}
+	pos++;
+	string username = "";
+	while (cur[pos] != ',')
+	{
+		username += cur[pos];
+		pos++;
+	}
+	return make_pair(place, username);
+}
+int main(int argc, char** argv)
+{
+	ifstream f1("list.txt");
+	string cur;
+	string lists = "";
+	int cid = 1;
+	while (getline(f1, cur))
+	{
+		if (cur == "!") break;
+		lists += cur + '\n';
+		cid++;
+	}
+	lists += argv[1];
+	f1.close();
+	ofstream f2("list.txt");
+	f2 << lists << "\n!\n";
+	f2.close();
+	map<string, int> dict;
+	ifstream f3("usernames.txt");
+	int uid = 1;
+	while (getline(f3, cur))
+	{
+		if (cur == "!") break;
+		dict[cur] = uid;
+		uid++;
+	}
+	f3.close();
+	ifstream f4(argv[2]);
+	ofstream f5("standings.txt");
+	getline(f4, cur);
+	f5 << cid << '\n';
+	int count = 0;
+	while (getline(f4, cur))
+	{
+		pair<int, string> info = dispose(cur);
+		if (info.first == 0) continue;
+		f5 << info.first << ' ' << dict[info.second] << '\n';
+		count++;
+	}
+	printf("OK! This is contest #%d. %d rated participants detected.", cid, count);
+	return 0;
+}
---
+++ fetch_usernames.js
@@ -0,0 +1,55 @@
+// Step 1: Create username dictionary
+// Command: node fetch_usernames.js
+import fs from 'fs'
+async function f(uid)
+{
+    const res =
+    await fetch
+    (
+        "http://8.136.99.126/api", 
+        {
+            headers:
+            [
+                ["content-type", "application/json"],
+                ["referer", "http://8.136.99.126/"],
+            ],
+            body:
+                JSON.stringify
+                (
+                    {
+                        "query":
+                            "query Example($id: Int!)\n" + 
+                            "{\n" + 
+                            "    user(id: $id)\n" + 
+                            "    {\n" + 
+                            "        uname\n" + 
+                            "    }\n" + 
+                            "}\n",
+                        "variables":
+                        {
+                            "id": uid
+                        },
+                        "operationName": "Example"
+                    }
+                ),
+            method: "POST",
+        }
+    );
+    if (!res.ok) return '';
+    const data = await res.json();
+    const user = data.data.user;
+    if (user == null) return '';
+    return user.uname;
+}
+fs.writeFileSync('usernames.txt', '');
+let names = await f(1);
+let index = 1;
+while (names != '')
+{
+    fs.appendFileSync('usernames.txt', `${names}\n`);
+    index = index + 1;
+    names = await f(index);
+}
+fs.appendFileSync('usernames.txt', `!\n`);
+index = index - 1;
+console.log(`OK! ${index} registered users detected.`);
