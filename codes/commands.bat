5ef30bc42645cbb77dd41baef803233924aad297
035966-L3 <139025318+035966-L3@users.noreply.github.com>
2025-08-24 09:38:06 +0800

    Remove API Requests

--- codes/commands.bat
+++ codes/commands.bat
@@ -1,6 +1,6 @@
 :: WARNINGS:
 :: These commands are for Windows only.
-:: You need Node.js and G++ tools to run them.
+:: You need G++ tools to run them.
 :: They should be run in the 'codes' directory.
 :: Remember to replace the string in brackets.
 :: Drag the CSV file here before running.
@@ -13,7 +13,6 @@ g++ 1_convert_csv_files.cpp -o 1_convert_csv_files
 g++ 2_calculate_performances.cpp -o 2_calculate_performances
 g++ 3_update_ratings_and_reports.cpp -o 3_update_ratings_and_reports
 g++ 4_roll_ranks_and_leaderboards.cpp -o 4_roll_ranks_and_leaderboards
-node ../fetch_usernames_irregularly.mjs
 1_convert_csv_files %1 %2
 2_calculate_performances %3
 3_update_ratings_and_reports
