39ddfb166de2b2dfd9b00ab97bda2a435dd5fe91
035966-L3 <2814139320@qq.com>
2026-01-26 00:30:15 +0800

    Update 0_generate_all_mongoshell_commands.cpp

--- codes/0_generate_all_mongoshell_commands.cpp
+++ codes/0_generate_all_mongoshell_commands.cpp
@@ -10,8 +10,13 @@ int main(int argc, char** argv)
 	freopen("update.txt", "ab", stdout);
 	argv[1][strlen(argv[1]) - 4] = '\0';
 	string s;
-	string t = "";
+	getline(cin, s);
+	string t = s + "\n", u = s;
 	while (getline(cin, s)) t = t + s + "\n";
+	printf("db.domain.user.updateOne( { domainId: 'system', uid: %s }, { $set: { \"level\": 0 } } );\n", argv[1] + 8);
+	printf("db.domain.user.updateOne( { domainId: 'system', uid: %s }, { $set: { \"rp\": %s } } );\n", argv[1] + 8, u.c_str());
+	printf("db.domain.user.updateOne( { domainId: 'system', uid: %s }, { $set: { \"rpInfo.problem\": 0 } } );\n", argv[1] + 8);
+	printf("db.domain.user.updateOne( { domainId: 'system', uid: %s }, { $set: { \"rpInfo.contest\": %s } } );\n", argv[1] + 8, u.c_str());
 	printf("db.domain.user.updateOne( { domainId: 'system', uid: %s }, { $set: { \"rpInfo.delta\": `%s` } } );\n", argv[1] + 8, t.c_str());
 	return 0;
 }
